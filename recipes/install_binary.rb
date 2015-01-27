47475e6516742b3ccee1b299474a586e6d774d32
Darron Froese <darron@froese.org>
2015-01-27 13:57:13 -0700

    Disabling so that it just installs the new versions.

--- recipes/install_binary.rb
+++ recipes/install_binary.rb
@@ -17,10 +17,12 @@
 
 include_recipe 'ark::default'
 
-install_arch = node['kernel']['machine'] =~ /x86_64/ ? 'amd64' : '386'
-install_version = [node['consul']['version'], node['os'], install_arch].join('_')
-install_checksum = node['consul']['checksums'].fetch(install_version)
+# Disabled until the final 0.5 release.
 
+# install_arch = node['kernel']['machine'] =~ /x86_64/ ? 'amd64' : '386'
+# install_version = [node['consul']['version'], node['os'], install_arch].join('_')
+# install_checksum = node['consul']['checksums'].fetch(install_version)
+#
 # ark 'consul' do
 #   path node['consul']['install_dir']
 #   version node['consul']['version']
